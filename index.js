973cb624a911fc344d61d65298aa29b5d262bafe
Monica <monicarigi@hotmail.com>
2019-09-04 11:46:52 +0700

    070104

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const items = require('./data')
 items.forEach(e => console.log(e))
+
+const calc = require('./calc')
+let prices = items.map(e => e.price)
+console.log(prices)
+console.log(calc.add(prices))
+console.log(calc.subtract(prices))
