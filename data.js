032594678ff799fe5b0e050d4e48a595e9a96f77
Monica <monicarigi@hotmail.com>
2019-09-04 11:36:28 +0700

    070101

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports = items
+
+let items = [
+	{name:'The Man Who Mistook His Wife for a Hat', price:195},
+	{name:'This is Going to Hurt', price:261},
+	{name:'The Power of Now', price:270},
+	{name:'Sapiens', price:479},
+	{name:'The Subtle Art of Not Giving a F*ck', price:466},
+	{name:'Think Punk', price:468},
+]
